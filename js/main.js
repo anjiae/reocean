aeeb478850aa35b65b7d42d4b784e639674b9fcc
anjiae <74403773+anjiae@users.noreply.github.com>
2020-12-31 23:19:45 +0900

    +section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 "use strict";
 
-const nextBtn = document.querySelector('.next');
-const prevBtn = document.querySelector('.prev');
+//map
+	var container = document.getElementById('map');
+	var options = {
+		center: new kakao.maps.LatLng(37.9685396476989, 128.759549653821),
+		level: 3
+  };
+  var map = new kakao.maps.Map(container, options);
+  //마커생성
+  var markerPosition  = new kakao.maps.LatLng(37.9685396476989, 128.759549653821); 
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
+
+	
 
 
